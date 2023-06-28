7dc1315516c0d1f7937062215a5a3d694c5c7471
satyam <satyam@gmail.com>
2023-06-28 23:35:21 +0530

    ready to use structure of home-signup-login pages

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home page</h1>
+      <Link to="/signup"><button>Signup</button></Link>
+      <Link to="/login"><button>Login</button></Link>
+    </div>
+  )
+}
