0814c265154ae486e7eac3f629292f8cffe63549
satyam <satyam@gmail.com>
2023-06-29 12:55:15 +0530

    email&pass

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if(email.trim === "" || password.trim === "") {
+      setError("Please enter both email and password");
+      return;
+    }
+    console.log("Email: ", email);
+    console.log("password: ", password);
+
+
+    //onSubmit 
+    setEmail("");
+    setPassword("");
+    setError("");
+  };
+
   return (
-    <div>Signup</div>
+    <div>
+      <h2>Signup Page</h2>
+      <form onSubmit={handleSignup}>
+
+        <div>
+          <label>Email:</label>
+          <input
+            type='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <input
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <button type='submit' disabled={email.trim === "" || password.trim === ""}>Signup</button>
+
+      </form>
+    </div>
   )
 }
